6dcc21a94dfe8c14ec3fbd48cc586f6ddc30616c
ErgodicMage <ErgodicMage@gmail.com>
2024-02-18 15:07:25 -0600

    Created Types folder and moved interfaces there.

---
+++ src/Components/Core/Types/IStellarMap.cs
@@ -0,0 +1,13 @@
+﻿namespace StellarMap.Core;
+
+internal interface IStellarMap
+{
+    string? Name { get; set; }
+    Dictionary<string, string>? MetaData { get; }
+    Dictionary<Identifier, Star>? Stars { get; set; }
+    Dictionary<Identifier, Planet>? Planets { get; set; }
+
+    Result<T> Get<T>(Identifier identifier) where T : IStellarObject;
+
+    Result Add<T>(T t) where T : IStellarObject;
+}
