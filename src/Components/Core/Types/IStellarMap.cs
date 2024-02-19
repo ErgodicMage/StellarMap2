2b3f58b8db67a2e9a2a86d8b16d330ab061cc388
ErgodicMage <ErgodicMage@gmail.com>
2024-02-18 20:20:01 -0600

    quick correction

--- src/Components/Core/Types/IStellarMap.cs
+++ src/Components/Core/Types/IStellarMap.cs
@@ -6,7 +6,7 @@ public interface IStellarMap
     Dictionary<string, string>? MetaData { get; }
     Dictionary<Identifier, Star>? Stars { get; set; }
     Dictionary<Identifier, Planet>? Planets { get; set; }
-    Dictionary<Identifier, Satelite>? Satelites { get; set; };
+    Dictionary<Identifier, Satelite>? Satelites { get; set; }
     Dictionary<Identifier, DwarfPlanet>? DwarfPlanets { get; set; }
 
     Result<T> Get<T>(Identifier identifier) where T : IStellarObject;
