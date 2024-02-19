987eb0ed6b31093f07144e2f4ad72c8ba01d35a7
ErgodicMage <ErgodicMage@gmail.com>
2024-02-18 22:52:16 -0600

    Added Add<T> and Get<T> functionality to StellarMap.

---
+++ src/Components/Core/Objects/StellarMap.cs
@@ -0,0 +1,97 @@
+﻿
+namespace StellarMap.Core;
+
+public class StellarMap : IStellarMap
+{
+    public string Name { get; set; }
+    public Dictionary<string, string> MetaData { get; }
+    public Dictionary<Identifier, Star>? Stars { get; set; }
+    public Dictionary<Identifier, Planet>? Planets { get; set; }
+    public Dictionary<Identifier, Satelite>? Satelites { get; set; }
+    public Dictionary<Identifier, DwarfPlanet>? DwarfPlanets { get; set; }
+    public Dictionary<Identifier, Asteroid>? Asteroids { get; set; }
+    public Dictionary<Identifier, Comet>? Comets { get; set; }
+
+    #region Constructors
+#pragma warning disable CS8618
+    public StellarMap() { }
+#pragma warning restore CS8618
+
+    public StellarMap(string name)
+    {
+        Name = name;
+        MetaData = new();
+    }
+    #endregion
+
+    public Result Add<T>(T t) where T : IStellarObject
+    {
+        var result = GuardClause.Null(t);
+        if (!result.Success) return result;
+
+        var dict = CreateDictionaryIfNeeded<T>();
+        if (!dict.Success) return dict;
+        
+        if (!dict.Value.ContainsKey(t.Identifier))
+            return Result.Error($"{t.Identifier} already exists in {nameof(T)}s");
+        
+        dict.Value.Add(t.Identifier, t);
+        return Result.Ok();
+    }
+
+    public Result<T> Get<T>(Identifier identifier) where T : IStellarObject
+    {
+        var result = GetDictionary<T>();
+        if (!result.Success) return Result<T>.Error($"Can not find {identifier} in {nameof(T)}s");
+
+        if (result.Value.TryGetValue(identifier, out var value))
+            return value;
+
+        return Result.Error($"Can not find {identifier} in {nameof(T)}s");
+    }
+
+    #region Dictionary Functions
+    protected virtual Result<Dictionary<Identifier, T>> GetDictionary<T>() where T : IStellarObject
+    {
+        var dictionary = default(T) switch
+        {
+            Star => Stars as Dictionary<Identifier, T>,
+            Planet => Planets as Dictionary<Identifier, T>,
+            DwarfPlanet => DwarfPlanets as Dictionary<Identifier, T>,
+            Satelite => Satelites as Dictionary<Identifier, T>,
+            Asteroid => Asteroids as Dictionary<Identifier, T>,
+            Comet => Comets as Dictionary<Identifier, T>,
+            _ => default
+        };
+
+        return dictionary!;
+    }
+
+    protected virtual Result<Dictionary<Identifier, T>> CreateDictionaryIfNeeded<T>() where T : IStellarObject
+    {
+        switch (default(T))
+        {
+            case Star:
+                Stars ??= new();
+                break;
+            case Planet:
+                Planets ??= new();
+                break;
+            case DwarfPlanet:
+                DwarfPlanets ??= new();
+                break;
+            case Satelite:
+                Satelites ??= new();
+                break;
+            case Asteroid:
+                Asteroids ??= new();
+                break;
+            case Comet:
+                Comets ??= new();
+                break;
+        };
+
+        return GetDictionary<T>();
+    }
+    #endregion
+}
--- src/Components/Core/Types/IStellarMap.cs
+++ src/Components/Core/Types/IStellarMap.cs
@@ -2,16 +2,16 @@
 
 public interface IStellarMap
 {
-    string? Name { get; set; }
-    Dictionary<string, string>? MetaData { get; }
+    string Name { get; set; }
+    Dictionary<string, string> MetaData { get; }
     Dictionary<Identifier, Star>? Stars { get; set; }
     Dictionary<Identifier, Planet>? Planets { get; set; }
-    Dictionary<Identifier, Satelite>? Satelites { get; set; }
     Dictionary<Identifier, DwarfPlanet>? DwarfPlanets { get; set; }
+    Dictionary<Identifier, Satelite>? Satelites { get; set; }
     Dictionary<Identifier, Asteroid>? Asteroids { get; set; }
     Dictionary<Identifier, Comet>? Comets { get; set; }
 
-    Result<T> Get<T>(Identifier identifier) where T : IStellarObject;
-
     Result Add<T>(T t) where T : IStellarObject;
+    
+    Result<T> Get<T>(Identifier identifier) where T : IStellarObject;
 }
