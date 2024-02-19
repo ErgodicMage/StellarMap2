f60dfb228f6e01f49197185230a8f4f3bab6fc4f
ErgodicMage <ErgodicMage@gmail.com>
2024-02-18 20:19:18 -0600

    Dooh for DwarfPlanet

--- src/Components/Core/Objects/DwarfPlanet.cs
+++ src/Components/Core/Objects/DwarfPlanet.cs
@@ -2,7 +2,25 @@
 
 public class DwarfPlanet : BaseStellarObject
 {
+    #region Properties
+    public IList<Identifier>? SateliteIdentifiers { get; set; }
+    #endregion
+
+    #region Constructors
     public DwarfPlanet() { }
 
     public DwarfPlanet(string name, Identifier identifier, IStellarMap map) : base(name, identifier, map, StellarObjectType.DwarfPlanet) { }
+    #endregion
+
+    #region Add
+    public Result Add(Satelite satelite) => AddSatelite(satelite);
+
+    public Result AddSatelite(Satelite satelite)
+    {
+        var result = GuardClause.Null(Map).Null(satelite);
+        if (!result.Success) return result;
+
+        return Map!.Add<Satelite>(satelite);
+    }
+    #endregion
 }
