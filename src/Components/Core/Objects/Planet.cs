f89187f0ec14907c331ad5a41ec64db7b28b9d4f
ErgodicMage <ErgodicMage@gmail.com>
2024-02-19 20:14:56 -0600

    Fixed Planet Add and Get Satelite

--- src/Components/Core/Objects/Planet.cs
+++ src/Components/Core/Objects/Planet.cs
@@ -3,13 +3,59 @@
 public class Planet : StellarObject, IStellarObject, IEqualityComparer<Planet>
 {
     #region Properties
-    public IList<Identifier>? SateliteIdentifiers { get; set; }
+    public List<Identifier>? SateliteIdentifiers { get; set; }
     #endregion
 
     #region Constructors
     public Planet() { }
 
     public Planet(string name, Identifier identifier, IStellarMap map) : base(name, identifier, map, StellarObjectType.Planet) { }
+    #endregion
+
+    #region Get
+    public Result<Satelite> GetSatelite(Identifier identifier) => Get(identifier);
+
+    public Result<Satelite> Get(Identifier identifier)
+    {
+        var result = GuardClause.Null(Map).Null(identifier);
+        if (!result.Success) return result;
+
+        if (SateliteIdentifiers is null)
+            return Result.Error($"No satelites exists for Planet {Name} {Identifier}");
+        return Map.Get<Satelite>(identifier);
+    }
+
+    public IList<Satelite> GetAllSatelites()
+    {
+        List<Satelite> satelites = new();
+
+        if (SateliteIdentifiers is null)
+            return satelites;
+
+        foreach (var identifier in SateliteIdentifiers)
+        {
+            Satelite? satelite = Map.Get<Satelite>(identifier);
+            if (satelite is not null)
+                satelites.Add(satelite);
+        }
+
+        return satelites;
+    }
+
+    public Result<Satelite> GetSateliteByName(string name)
+    {
+        var result = GuardClause.Null(Map).NullOrWhiteSpace(name);
+        if (!result.Success) return result;
+
+        IList<Satelite> satelites = GetAllSatelites();
+        
+        var satelite = satelites.FirstOrDefault(s => s.Name == name);
+
+        if (satelite is null)
+            return Result.Error($"Satelite {name} not found for Planet {Name} {Identifier}");
+
+        return satelite;
+    }
     #endregion
 
     #region Add
@@ -20,7 +66,20 @@ public class Planet : StellarObject, IStellarObject, IEqualityComparer<Planet>
         var result = GuardClause.Null(Map).Null(satelite);
         if (!result.Success) return result;
 
-        return Map!.Add<Satelite>(satelite);
+        if (SateliteIdentifiers is null)
+            SateliteIdentifiers = new();
+
+        var exists = SateliteIdentifiers.Any(i => i == satelite.Identifier);
+
+        if (exists)
+            return Result.Error($"{satelite.Name} {satelite.Identifier} already exists for Planet {Name} {Identifier}");
+
+        result = Map!.Add<Satelite>(satelite);
+
+        if (result)
+            SateliteIdentifiers.Add(satelite.Identifier);
+
+        return result;
     }
     #endregion
 
