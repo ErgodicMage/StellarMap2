e7598ceb7d9dcafec2374c1f29c44b90e42ffd13
ErgodicMage <ErgodicMage@gmail.com>
2024-02-21 19:51:27 -0600

    Added Create functions to StellarObjectBuilder

--- src/Components/Core/Builders/StellarObjectBuilder.cs
+++ src/Components/Core/Builders/StellarObjectBuilder.cs
@@ -37,4 +37,32 @@ public abstract class StellarObjectBuilder
         _result = stellarObject.Add<T>(stellarObject);
         return this;
     }
+
+    #region Create Builders
+    public static StarBuilder CreateStar(string name, IIdentifierGenerator generator, IStellarMap map)
+        => StarBuilder.Create(name, generator, map);
+    public static StarBuilder CreateStar(string name, Identifier identifier, IStellarMap map)
+        => StarBuilder.Create(name, identifier, map);
+
+    public static PlanetBuilder CreatePlanet(string name, IIdentifierGenerator generator, IStellarMap map)
+    => PlanetBuilder.Create(name, generator, map);
+    public static PlanetBuilder CreatePlanet(string name, Identifier identifier, IStellarMap map)
+        => PlanetBuilder.Create(name, identifier, map);
+
+    public static SateliteBuilder CreateSatelite(string name, IIdentifierGenerator generator, IStellarMap map)
+    => SateliteBuilder.Create(name, generator, map);
+    public static SateliteBuilder CreateSatelite(string name, Identifier identifier, IStellarMap map)
+        => SateliteBuilder.Create(name, identifier, map);
+
+    public static AsteroidBuilder CreateAsteroid(string name, IIdentifierGenerator generator, IStellarMap map)
+    => AsteroidBuilder.Create(name, generator, map);
+    public static AsteroidBuilder CreateAsteroid(string name, Identifier identifier, IStellarMap map)
+        => AsteroidBuilder.Create(name, identifier, map);
+
+    public static CometBuilder CreateComet(string name, IIdentifierGenerator generator, IStellarMap map)
+    => CometBuilder.Create(name, generator, map);
+
+    public static CometBuilder CreateComet(string name, Identifier identifier, IStellarMap map)
+        => CometBuilder.Create(name, identifier, map);
+    #endregion
 }
