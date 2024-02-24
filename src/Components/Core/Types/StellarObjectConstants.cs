a8d1d5996e1d9bd7ae86feb804a9c2d95006906b
ErgodicMage <ErgodicMage@gmail.com>
2024-02-24 09:29:47 -0600

    Removed remaining references to DwarfPlanet

--- src/Components/Core/Types/StellarObjectConstants.cs
+++ src/Components/Core/Types/StellarObjectConstants.cs
@@ -6,7 +6,6 @@ public static class StellarObjectConstants
     public const string Star = "Star";
     public const string Planet = "Planet";
     public const string Satellite = "Satellite";
-    public const string DwarfPlanet = "DwarfPlanet";
     public const string Asteroid = "Asteroid";
     public const string Comet = "Comet";
 }
