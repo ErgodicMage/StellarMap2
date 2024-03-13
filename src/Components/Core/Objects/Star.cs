fb0e5249eaa0f3ac4952321e0431d1d124100e62
ErgodicMage <ErgodicMage@gmail.com>
2024-03-13 14:02:40 -0500

    Made all properties in Star nullable

--- src/Components/Core/Objects/Star.cs
+++ src/Components/Core/Objects/Star.cs
@@ -13,13 +13,13 @@ public class Star : StellarObject, IStellarObject, IEqualityComparer<Star>
     public Dictionary<string, Identifier>? Planets {get; set; }
 
     [JsonPropertyOrder(13)]
-    public Dictionary<string, Identifier> DwarfPlanets { get; set; }
+    public Dictionary<string, Identifier>? DwarfPlanets { get; set; }
 
     [JsonPropertyOrder(14)]
-    public Dictionary<string, Identifier> Asteroids {get; set; }
+    public Dictionary<string, Identifier>? Asteroids {get; set; }
 
     [JsonPropertyOrder(15)]
-    public Dictionary<string, Identifier> Comets {get; set; }
+    public Dictionary<string, Identifier>? Comets {get; set; }
 
     #endregion
 
