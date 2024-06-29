a12fd513bc257315d1021ed9dfa59eeb2c426f27
ErgodicMage <ErgodicMage@gmail.com>
2024-06-29 17:27:20 -0500

    Updated Result package to 0.4.0

--- src/StellarSystems/Progression/GlobalUsing.cs
+++ src/StellarSystems/Progression/GlobalUsing.cs
@@ -4,7 +4,6 @@ global using System.Linq;
 global using System.Text;
 global using System.Text.Json.Serialization;
 
-global using Ardalis.SmartEnum;
 global using ErgodicMage.Result;
 
 global using StellarMap.Core;
