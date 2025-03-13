1a171ffec1298d9d430459e82110881b328d2897
ErgodicMage <ErgodicMage@gmail.com>
2025-03-13 15:14:52 -0500

    Updated to net9.0 fixed build props issues

--- src/Applications/PhysicalStellarMap/GlobalUsing.cs
+++ src/Applications/PhysicalStellarMap/GlobalUsing.cs
@@ -1,3 +1,7 @@
-﻿global using ErgodicMage.Result;
+﻿global using System;
+global using System.IO;
+global using System.Text.Json;
+
+global using ErgodicMage.Result;
 global using StellarMap.Core;
 global using StellarMap.PhysicalStellarBuilder;
--- src/Applications/PhysicalStellarMap/Program.cs
+++ src/Applications/PhysicalStellarMap/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System.Text.Json;
 
 IConfiguration config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
